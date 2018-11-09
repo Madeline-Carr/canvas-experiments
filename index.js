fc884064f476e797b35ad815cb30fce4de237c97
Madeline Carr <Carrm2@vcu.edu>
2018-11-08 23:22:55 -0500

    Shapes and Movement
    These projects demonstrate how to make and move shapes.

---
+++ index.js
@@ -0,0 +1,150 @@
+var canvas = document.querySelector("canvas");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext("2d");
+/*
+c.fillStyle = "rgba(255, 140, 250 ,0.5)";
+c.fillRect(300, 30, 100, 100);
+c.fillStyle = "rgba(155, 140, 150 ,0.5)";
+c.fillRect(400, 30, 100, 100);
+//c.fillRect(550, 100, 100, 100);
+//c.fillRect(200, 400, 400, 300);
+console.log(canvas);
+
+//Lines
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(100, 500);
+c.lineTo(400, 800);
+c.strokeStyle = "#333";
+c.stroke();
+
+
+//Arcs and Circles
+c.beginPath();
+c.strokeStyle = "aliceblue";
+c.arc(300, 400, 30, 0, Math.PI * 2, false);
+c.stroke();
+
+//Making many circles
+for (let i = 0; i < 50; i++) {
+  var x = Math.random() * window.innerWidth;
+  var y = Math.random() * window.innerHeight;
+  c.beginPath();
+  c.strokeStyle = "aliceblue";
+  c.arc(x, y, 30, 0, Math.PI * 2, false);
+  c.stroke();
+}
+
+//Chalk line
+var x = 200;
+function animate() {
+  requestAnimationFrame(animate);
+  c.beginPath();
+  c.strokeStyle = "aliceblue";
+  c.arc(x, 400, 30, 0, Math.PI * 2, false);
+  c.stroke();
+  x += 2;
+}
+animate();
+
+//Adding circle movement
+var x = 200;
+var dx = 1;
+var y = 200;
+var dy = 4;
+var radius = 30;
+function animate() {
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  requestAnimationFrame(animate);
+  c.beginPath();
+  c.strokeStyle = "aliceblue";
+  c.arc(x, y, radius, 0, Math.PI * 2, false);
+  c.stroke();
+  if (x + radius > innerWidth || x - radius < 0) {
+    dx = -dx;
+  }
+  if (y + radius > innerHeight || y - radius < 0) {
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
+}
+animate();
+
+//Adding circle movement in diffrent directions and speeds
+var x = Math.random() * innerWidth;
+var dx = (Math.random() - 0.5) * 9;
+var y = Math.random() * innerHeight;
+var dy = (Math.random() - 0.5) * 9;
+var radius = 30;
+function animate() {
+  c.clearRect(0, 0, innerWidth, innerHeight);
+  requestAnimationFrame(animate);
+  c.beginPath();
+  c.strokeStyle = "aliceblue";
+  c.arc(x, y, radius, 0, Math.PI * 2, false);
+  c.stroke();
+  if (x + radius > innerWidth || x - radius < 0) {
+    dx = -dx;
+  }
+  if (y + radius > innerHeight || y - radius < 0) {
+    dy = -dy;
+  }
+  x += dx;
+  y += dy;
+}
+animate();
+*/
+
+//Object Circles
+
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.draw = function() {
+    c.beginPath();
+    c.strokeStyle = "aliceblue";
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.stroke();
+  };
+  this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+  };
+}
+
+var circleArray = [];
+
+for (let i = 0; i < 100; i++) {
+  var radius = 30;
+  var x = Math.random() * (innerWidth - radius * 2) + radius;
+  var dx = (Math.random() - 0.5) * 9;
+  var y = Math.random() * innerHeight;
+  var dy = (Math.random() - 0.5) * 9;
+
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+console.log(circleArray);
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, innerWidth, innerHeight);
+
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
+}
+animate();
