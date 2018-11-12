707289192503fe8381d7420831f07d9640e7d190
Madeline Carr <Carrm2@vcu.edu>
2018-11-11 23:30:41 -0500

    Created Header, Added Comments, & More Color Features
    I added a header with a title, commented the code, and added a way to change the colors every time they hit a side.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Selects and sizes the canvas.
 var canvas = document.querySelector("canvas");
 
 canvas.width = window.innerWidth;
@@ -5,8 +6,7 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext("2d");
 
-//My experimentation: The circles turn white when they hit the top or bottom walls and black when they hit the right or left.
-
+//Circle Object Constructor
 function Circle(x, y, dx, dy, radius, chosenColor) {
   this.x = x;
   this.y = y;
@@ -26,12 +26,25 @@ function Circle(x, y, dx, dy, radius, chosenColor) {
     if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
       this.dx = -this.dx;
       this.color = "black";
-      this.radius = this.radius / 1.2;
+      /*This will change the color each time it hits a side. 
+      The second time it will turn tomato.
+      if (this.color === "black") {
+        this.color = "#tomato";
+      } else this.color = "black";
+      */
+      //this.radius = this.radius / 1.2;
     }
     if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
       this.dy = -this.dy;
       this.color = "white";
-      this.radius = this.radius / 1.2;
+      /*This will change the color each time it hits the top or bottom. 
+      The second time it will turn blue.
+      if (this.color === "white") {
+        this.color = "#00ADB5";
+      } else this.color = "white";
+      */
+
+      //this.radius = this.radius / 1.2;
     }
     this.x += this.dx;
     this.y += this.dy;
@@ -39,22 +52,25 @@ function Circle(x, y, dx, dy, radius, chosenColor) {
   };
 }
 
+//Makes an array to store the circles in
 var circleArray = [];
-
-for (let i = 0; i < 500; i++) {
+//Makes the circles
+for (let i = 0; i < 600; i++) {
   var radius = Math.floor(Math.random() * 30);
   var x = Math.random() * (innerWidth - radius * 2) + radius;
-  var dx = (Math.random() - 0.5) * 9;
+  var dx = (Math.random() - 0.5) * 10;
   var y = Math.random() * innerHeight;
-  var dy = (Math.random() - 0.5) * 9;
+  var dy = (Math.random() - 0.5) * 10;
   var colors = ["#00ADB5", "#FFF4E0", "#393E46", "#F8B500", "#FC3C3C"];
   var colorNumber = Math.floor(Math.random() * 5);
   var chosenColor = colors[colorNumber];
 
   circleArray.push(new Circle(x, y, dx, dy, radius, chosenColor));
 }
+//Logs the circles
 console.log(circleArray);
 
+//Animates the circles
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
